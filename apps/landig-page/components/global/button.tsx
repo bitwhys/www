c5471533a73f26f3cba3b97f2f8a12a079703cfe
George Bockari <bitwhys@gmail.com>
2024-09-13 21:36:36 -0400

    style: prettier

--- apps/landig-page/components/global/button.tsx
+++ apps/landig-page/components/global/button.tsx
@@ -1,34 +1,42 @@
-import clsx from "clsx";
-import Link from "next/link";
-import type { ButtonProps, ButtonVariant } from "@/types";
+import clsx from 'clsx'
+import Link from 'next/link'
+import type { ButtonProps, ButtonVariant } from '@/types'
 
 const ButtonVariant: ButtonVariant = {
-    primary: "btn--primary",
-    secondary: "btn--secondary",
-    outline: "btn--outline",
-    black: "btn--black",
-    white: "btn--white",
-    text: "btn--text"
-};
+  primary: 'btn--primary',
+  secondary: 'btn--secondary',
+  outline: 'btn--outline',
+  black: 'btn--black',
+  white: 'btn--white',
+  text: 'btn--text',
+}
 
-export const Button: React.FC<ButtonProps> = ({ children, link = "", action = "button", variant = "primary", className = "", mode = "link", target = "_self" }: ButtonProps) => {
-    const Element = mode === "button" ? "button" : Link;
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  link = '',
+  action = 'button',
+  variant = 'primary',
+  className = '',
+  mode = 'link',
+  target = '_self',
+}: ButtonProps) => {
+  const Element = mode === 'button' ? 'button' : Link
 
-    const commonAttributes = {
-        role: "button",
-        href: link ? link : "#",
-        target: mode === "link" ? target : "_self",
-        className: clsx("btn", ButtonVariant[variant], className)
-    };
+  const commonAttributes = {
+    role: 'button',
+    href: link ? link : '#',
+    target: mode === 'link' ? target : '_self',
+    className: clsx('btn', ButtonVariant[variant], className),
+  }
 
-    const buttonAttributes = {
-        ...(mode === "button" && action ? { type: action } : {})
-    };
+  const buttonAttributes = {
+    ...(mode === 'button' && action ? { type: action } : {}),
+  }
 
-    const attributes = {
-        ...commonAttributes,
-        ...(mode === "button" ? buttonAttributes : {})
-    };
+  const attributes = {
+    ...commonAttributes,
+    ...(mode === 'button' ? buttonAttributes : {}),
+  }
 
-    return <Element {...attributes}>{children}</Element>;
-};
+  return <Element {...attributes}>{children}</Element>
+}
